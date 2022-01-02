932b79e7c4c83ac65760b5664fd7c7747997b457
rabishh <rabishyoung@gmail.com>
2022-01-02 18:35:35 +0530

    galary

---
+++ index.js
@@ -0,0 +1,25 @@
+let btn=document.querySelector('.img');
+btn.addEventListener("click" , function(){
+    btn.classList.toggle("img2");
+    console.log("clicked");
+})
+let btn2=document.querySelector(".img3")
+btn2.addEventListener("click" , function(){
+    btn2.classList.toggle("img2");
+    console.log("clicked");
+})
+let btn3=document.querySelector('.img4');
+btn3.addEventListener("click" , function(){
+    btn3.classList.toggle("img2");
+    console.log("clicked");
+})
+let btn4=document.querySelector('.img5');
+btn4.addEventListener("click" , function(){
+    btn4.classList.toggle("img2");
+    console.log("clicked");
+})
+let btn5=document.querySelector('.img6');
+btn5.addEventListener("click" , function(){
+    btn5.classList.toggle("img2");
+    console.log("clicked");
+})
